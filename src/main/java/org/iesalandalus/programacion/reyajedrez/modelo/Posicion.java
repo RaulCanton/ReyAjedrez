26f5ae27c4b02ace378edf9251796d910ccc8922
Raúl Cantón <raulcantong@hotmail.com>
2023-11-19 14:07:46 +0100

    Añadido mi nombre a readme Dirección Color Atributos Posicion Posicion completa

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -12,6 +12,9 @@ public class Posicion {
             throw new IllegalArgumentException("La fila introducida no es valida");
     }
 
+    public int getFila(){
+        return fila;
+    }
     public void setColumna (char columna){
         if (columna>='a' && columna<='h')
             this.columna=columna;
@@ -19,9 +22,53 @@ public class Posicion {
             throw new IllegalArgumentException("La columna introducida no es valida");
     }
 
+    public char getColumna(){
+        return columna;
+    }
     public Posicion(int fila,char columna){
         setFila(fila);
         setColumna(columna);
 
+    }
+
+    public Posicion(Posicion posicion){
+        setFila(posicion.fila);
+        setColumna(posicion.columna);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Posicion other = (Posicion) obj;
+        if (this.fila != other.fila) {
+            return false;
+        }
+        if (this.columna != other.columna) {
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
+    private int hashCode() {
+      int hash = 7;
+      hash = 17 * hash + fila;
+      hash = 17 * hash + columna;
+      return hash;
+    }
+
+    @Override
+
+    public String toString(){
+        return ("fila= "+ getFila()+" columna= "+getColumna());
     }
 }
