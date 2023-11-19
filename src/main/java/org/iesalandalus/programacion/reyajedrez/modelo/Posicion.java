169fbfed0510bd0b88d56f8ef969a9408f9fe88a
Raúl Cantón <raulcantong@hotmail.com>
2023-11-19 12:42:59 +0100

    Añadido mi nombre a readme Dirección Color Atributos Posicion

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -4,4 +4,19 @@ public class Posicion {
 
     private int fila;
     private char columna;
+
+    public void setFila (int fila){
+        if (fila>=1 && fila<=8)
+            this.fila=fila;
+        else
+            throw new IllegalArgumentException("La fila introducida no es valida");
+    }
+
+    public void setColumna (char columna){
+        if (columna>='a' && columna<='h')
+            this.columna=columna;
+        else
+            throw new IllegalArgumentException("La columna introducida no es valida");
+    }
+
 }
