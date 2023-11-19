e8b82cfbd1cae5916219f4587ef129e543c6418a
Raúl Cantón <raulcantong@hotmail.com>
2023-11-19 18:25:28 +0100

    Añadido mi nombre a readme Dirección Color Atributos Posicion Posicion completa Rey atributos

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -6,5 +6,14 @@ public class MainApp {
     public static void main(String[] args) {
 
         private Rey rey;
+
+
+        private ejecutarOpcion(){
     }
-}
+
+    private crearReyDefecto(){
+    }
+    private mover(){
+
+        }
+
