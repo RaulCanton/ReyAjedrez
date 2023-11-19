23b893d1d109f33ffb748381b195193c5bd9df11
Raúl Cantón <raulcantong@hotmail.com>
2023-11-19 19:55:00 +0100

    Añadido mi nombre a readme Dirección Color Atributos Posicion Posicion completa Rey atributos

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Direccion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Direccion.java
@@ -1,7 +1,18 @@
 package org.iesalandalus.programacion.reyajedrez.modelo;
 
 public enum Direccion {
-    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE, ENROQUE_CORTO, ENROQUE_LARGO
+    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE, ENROQUE_CORTO, ENROQUE_LARGO;
 
+    private String cadenaAMostrar;
+
+    private String Direccion(String cadenaAMostrar){
+        this.cadenaAMostrar=cadenaAMostrar;
+    }
+
+    @Override
+
+    public String toString(){
+        return cadenaAMostrar;
+    }
 
 }
